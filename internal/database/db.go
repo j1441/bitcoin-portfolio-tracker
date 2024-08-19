2e78cf27ec51bbf0584e934235db57dcc747912c
j1441 <95598987+j1441@users.noreply.github.com>
2024-08-20 01:02:03 +0200

    db url

--- internal/database/db.go
+++ internal/database/db.go
@@ -13,7 +13,7 @@ func ConnectDB() *sql.DB {
 	var err error
 
 	// Get the database URL from the environment variable
-	databaseUrl := os.Getenv("postgres://u8jks8baoetphi:p0c2176fb806d0f9b73959a75f12006ae4829cf910bfef8b5b04f8138dd177123@ceqbglof0h8enj.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dat3dvea3hjegd")
+	databaseUrl := os.Getenv("DATABASE_URL")
 
 	if databaseUrl == "" {
 		log.Fatal("DATABASE_URL is not set")
