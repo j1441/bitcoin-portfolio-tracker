ca686bf24e8287b64126b91aeb872870b0d729a3
j1441 <95598987+j1441@users.noreply.github.com>
2024-08-19 23:23:00 +0200

    Deployed to Heroku.

--- internal/database/db.go
+++ internal/database/db.go
@@ -3,21 +3,34 @@ package database
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func ConnectDB() *sql.DB {
-	connStr := "user=satoshi password=nakamoto dbname=bitcoin_portfolio_tracker sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
+	var db *sql.DB
+	var err error
+
+	// Get the database URL from the environment variable
+	databaseUrl := os.Getenv("postgres://u8jks8baoetphi:p0c2176fb806d0f9b73959a75f12006ae4829cf910bfef8b5b04f8138dd177123@ceqbglof0h8enj.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dat3dvea3hjegd")
+
+	if databaseUrl == "" {
+		log.Fatal("DATABASE_URL is not set")
+	}
+
+	// Connect to the database
+	db, err = sql.Open("postgres", databaseUrl)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error opening database: %q", err)
 	}
 
-	if err := db.Ping(); err != nil {
-		log.Fatal(err)
+	// Test the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %q", err)
 	}
 
-	log.Println("Connected to the database")
+	log.Println("Successfully connected to the database")
 	return db
 }
